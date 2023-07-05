0370370fe64a390c1c0337b45d889eed8dd3e2fd
Mrchuki <ivanjudez9@gmail.com>
2023-07-05 16:40:22 +0200

    Remove comments

--- src/main/java/com/example/distributed_systems/DistributedSystemsApplication.java
+++ src/main/java/com/example/distributed_systems/DistributedSystemsApplication.java
@@ -2,8 +2,14 @@ package com.example.distributed_systems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan({"com.example.distributed_systems.controller"})
+@EntityScan("com.example.distributed_systems.model")
+@EnableJpaRepositories("com.example.distributed_systems.repository")
 public class DistributedSystemsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DistributedSystemsApplication.class, args);
--- src/main/java/com/example/distributed_systems/repository/EmployeeRepository.java
+++ src/main/java/com/example/distributed_systems/repository/EmployeeRepository.java
@@ -1,11 +1,10 @@
 package com.example.distributed_systems.repository;
 
 import com.example.distributed_systems.model.Employee;
-//import org.springframework.data.repository.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository;
 
-@org.springframework.stereotype.Repository
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 
 }
